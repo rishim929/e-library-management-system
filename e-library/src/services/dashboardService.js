ecaa220f469588986a446b848831c102f0eb360e
rishim929 <rishimsapkota@gmail.com>
2026-07-30 12:28:06 +0545

    feat: implement API service modules for category, dashboard, notification, payment, reading history, subscription, and user management

--- e-library/src/services/dashboardService.js
+++ e-library/src/services/dashboardService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 import { API_BASE_URL } from "../config";
-const API = `${API_BASE_URL}/api/books`;
+const API = `${API_BASE_URL}/api/dashboard`;
 // Books
 export const getBookCount = () =>
   axios.get(`${API}/books`);
